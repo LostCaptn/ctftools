0ab0cb6dc6d042fe7a1d38a98e17b50e6734fb10
LostCaptn <c.nelson.ap@gmail.com>
2025-01-30 16:45:38 +0100

    Organizing

--- ctftools.py
+++ ctftools.py
@@ -1,7 +1,7 @@
 """
 Christian Nelson
 22 Jan 2025
-Convert binary octets into decimal
+Common CTF Cipher Decoder
 """
 import base64
 import codecs
@@ -29,57 +29,36 @@ def options():
     """
     while True:
         print(menu)
-        option = input("Enter a number: ")
-        if option == "1":
+        action = input("Enter a number: ")
+        if action == "1":
             base32main()
             break
-        elif option == "2":
+        elif action == "2":
             base64main()
             break
-        elif option == "3":
+        elif action == "3":
             ipv4decoder()
             break
-        elif option == "4":
+        elif action == "4":
             subciphermain()
             break
-        elif option == "5":
+        elif action == "5":
             hexmain()
             break
-        elif option == "6":
+        elif action == "6":
             rot_n_main()
             break
-        elif option == "7":
+        elif action == "7":
             bruteforcermain()
-        elif option == "8":
+        elif action == "8":
             return
         else:
             print("I'm sorry, that option isn't available right now")
     uestools()
 
-def bruteforcermain():
-    string = input("Enter a string to be tested: ")
-    result = base32decoder(string)
-    print(f"Base32: {result}")
-    result = base64decoder(string)
-    print(f"Base64: {result}")
-    result = rot13decode(string)
-    print(f"ROT13: {result}")
-    result = rot47decode(string)
-    print(f"ROT47: {result}")
-    result = hex2dec(string)
-    print(f"Hex2dec: {result}")
-    result = hex2ascii(string)
-    print(f"Hex2ASCII: {result}")
-    while True:
-        action = input("Would you like to view the substitution brute force? Y/n: ")
-        if action in ["Y" , "y"]:
-            subbruteforce(string)
-            break
-        elif action == "n":
-            break
-        else:
-            print("That's not a valid option")
-    return
+
+
+#---------------------------------------------DECODERS---------------------------------------------------------------
 
 def base64decoder(string):
     try:
@@ -93,71 +72,11 @@ def base64encoder(string):
     except Exception as e:
         return e
 
-
-def base64main():
-    action = input("Choose an option: \n 1. Encode \n 2. Decode \n 3. Exit \n")
-    if action == "1":
-        string = input("Enter the string to encode: ")
-        encoded = base64encoder(string)
-        print(f"Encoded string: {encoded}")
-        base64main()
-    elif action == "2":
-        string = input("Enter the string to decode: ")
-        decoded = base64decoder(string)
-        print(f"Decoded string: {decoded}")
-        base64main()
-    elif action == "3":
-        print("Goodbye")
-    else:
-        print("I'm sorry, that's not a valid option.")
-        base64main()
-
 def base32decoder(string):
     try:
         return base64.b32decode(string.encode('ascii')).decode('ascii')
     except Exception as e:
         return e
-
-def base32main():
-    while True:
-        string = input("Enter a base32 string to decode: ")
-        decoded = base32decoder(string)
-        print(f"Decoded string: {decoded}")
-
-        return
-
-
-def ipv4decoder():
-    new_octet = octetfix() #assigns the output to a variable
-    first_octet = convert2decimal(new_octet) #assigns variable to the output of new_octet being passed into converter
-    print(first_octet)
-    new_octet = octetfix()
-    second_octet = convert2decimal(new_octet)
-    print(second_octet)
-    new_octet = octetfix()
-    third_octet = convert2decimal(new_octet)
-    print(third_octet)
-    new_octet = octetfix()
-    fourth_octet = convert2decimal(new_octet)
-    print(fourth_octet)
-    print(f"The IPV4 address is {first_octet}.{second_octet}.{third_octet}.{fourth_octet}")
-
-def octetfix(): #validates the input
-    while True:
-        octet_str = input("Enter the octet: ")
-        try:
-            octettest = int(octet_str, 2) #verifies that the string can convert to binary
-        except ValueError:
-            print("That is not a binary number, please input only binary.")
-        if len(octet_str) == 8: #verifies that the input is exactly 8 characters
-            break
-        else:
-            print("That is not a valid octet")
-    return octet_str #returns our input to pass on later
-
-def convert2decimal(new_octet):
-    new_dec = int(new_octet, 2) #converts the binary to decimal form using int()
-    return new_dec #returns the input to pass on for printing
 
 def subbruteforce(string):
     library = "abcdefghijklmnopqrstuvwxyz"
@@ -257,40 +176,29 @@ def subkeyencript(string):
     except Exception as e:
         return e
 
-def subciphermain():
-    string = input("Enter the string: ")
-    action = input("Enter a Number: \n 1. Encode \n 2. Decode \n 3. Brute Force \n 4. Exit \n Option: ")
-    if action == "1":
-        result = subkeyencript(string)
-        print(f"Your encrypted message: {result}")
-        subciphermain()
-    elif action == "2":
-        result = subkeydecript(string)
-        print(f"Your decrypted message is: {result}")
-        subciphermain()
-    elif action == "3":
-        subbruteforce(string)
-        subciphermain()
-    elif action == "4":
-        print("Goodbye")
-    else:
-        print("I'm sorry, that's not a valid option.")
-        subciphermain()
-
-def hexvalidator():
-    while True:
-        hex_string = input("Enter a hexadecimal string: ")
-        temp = hex_string
-        hex_string = hex_string.replace(".", " ")
-        hex_string = hex_string.replace(" ", "")
-        hex_string = hex_string.lower()
-        try:
-            hextest = int(hex_string, 16) #verifies that the string can convert to hex
-            hex_string = temp
-            break
-        except ValueError:
-            print("That is not a hex number, please input only hex.")
-    return hex_string  # returns our input to pass on later
+def hex2ascii(string):
+    library = "0123456789abcdef"
+    dec_string = ""
+    hex_string = string
+    hex_string = hex_string.replace(".", " ")
+    temp = ""
+    try:
+        for i in hex_string:
+            if i in library:
+                temp = temp + i
+                hex_string = hex_string.replace(i, "", 1)
+            if i not in library:
+                temp = chr(int(temp, 16))
+                dec_string = dec_string + str(temp)
+                temp = ""
+                hex_string = hex_string.replace(" ", "", 1)
+            if hex_string == "":
+                temp = chr(int(temp, 16))
+                dec_string = dec_string + str(temp)
+                break
+        return dec_string
+    except Exception as e:
+        return e
 
 def hex2dec(string):
     library = "0123456789abcdef"
@@ -316,30 +224,146 @@ def hex2dec(string):
     except Exception as e:
         return e
 
-def hex2ascii(string):
-    library = "0123456789abcdef"
-    dec_string = ""
-    hex_string = string
-    hex_string = hex_string.replace(".", " ")
-    temp = ""
+def rot13decode(string):
     try:
-        for i in hex_string:
-            if i in library:
-                temp = temp + i
-                hex_string = hex_string.replace(i, "", 1)
-            if i not in library:
-                temp = chr(int(temp, 16))
-                dec_string = dec_string + str(temp)
-                temp = ""
-                hex_string = hex_string.replace(" ", "", 1)
-            if hex_string == "":
-                temp = chr(int(temp, 16))
-                dec_string = dec_string + str(temp)
-                break
-        return dec_string
+        return codecs.encode(string, 'rot13')
+    except Exception as e:
+        return f"Invalid Input: {e}"
+
+def rot47decode(string):
+    try:
+        x = []
+        for i in range(len(string)):  # access characters by index
+            j = ord(string[i])  # set j to the corresponding ordinal number
+            if 33 <= j <= 126:  # checks for ROT47 range
+                # decode left 47, account for negative 33, wrap %94
+                x.append(chr(33 + ((j - 47 - 33) % 94)))
+            else:
+                x.append(string[i])  #adds non ROT47 characters without changes
+        return ''.join(x)  # turns the list into a string
     except Exception as e:
         return e
 
+#--------------------------------------------------VALIDATORS----------------------------------------------------------
+
+def octetvalidator(): #validates the input
+    while True:
+        octet_str = input("Enter the octet: ")
+        try:
+            octettest = int(octet_str, 2) #verifies that the string can convert to binary
+        except ValueError:
+            print("That is not a binary number, please input only binary.")
+        if len(octet_str) == 8: #verifies that the input is exactly 8 characters
+            break
+        else:
+            print("That is not a valid octet")
+    return octet_str #returns our input to pass on later
+
+def hexvalidator():
+    while True:
+        hex_string = input("Enter a hexadecimal string: ")
+        temp = hex_string
+        hex_string = hex_string.replace(".", " ")
+        hex_string = hex_string.replace(" ", "")
+        hex_string = hex_string.lower()
+        try:
+            hextest = int(hex_string, 16) #verifies that the string can convert to hex
+            hex_string = temp
+            break
+        except ValueError:
+            print("That is not a hex number, please input only hex.")
+    return hex_string  # returns our input to pass on later
+
+
+#--------------------------------------------------MAIN FUNCTIONS_______________________________________________________
+def bruteforcermain():
+    string = input("Enter a string to be tested: ")
+    result = base32decoder(string)
+    print(f"Base32: {result}")
+    result = base64decoder(string)
+    print(f"Base64: {result}")
+    result = rot13decode(string)
+    print(f"ROT13: {result}")
+    result = rot47decode(string)
+    print(f"ROT47: {result}")
+    result = hex2dec(string)
+    print(f"Hex2dec: {result}")
+    result = hex2ascii(string)
+    print(f"Hex2ASCII: {result}")
+    while True:
+        action = input("Would you like to view the substitution brute force? Y/n: ")
+        if action in ["Y" , "y"]:
+            subbruteforce(string)
+            break
+        elif action == "n":
+            break
+        else:
+            print("That's not a valid option")
+    return
+
+def base32main():
+    while True:
+        string = input("Enter a base32 string to decode: ")
+        decoded = base32decoder(string)
+        print(f"Decoded string: {decoded}")
+        return
+
+def base64main():
+    action = input("Choose an option: \n 1. Encode \n 2. Decode \n 3. Exit \n")
+    if action == "1":
+        string = input("Enter the string to encode: ")
+        encoded = base64encoder(string)
+        print(f"Encoded string: {encoded}")
+        base64main()
+    elif action == "2":
+        string = input("Enter the string to decode: ")
+        decoded = base64decoder(string)
+        print(f"Decoded string: {decoded}")
+        base64main()
+    elif action == "3":
+        print("Goodbye")
+    else:
+        print("I'm sorry, that's not a valid option.")
+        base64main()
+
+def ipv4decoder():
+    new_octet = octetvalidator() #assigns the output to a variable
+    first_octet = convert2decimal(new_octet) #assigns variable to the output of new_octet being passed into converter
+    print(first_octet)
+    new_octet = octetvalidator()
+    second_octet = convert2decimal(new_octet)
+    print(second_octet)
+    new_octet = octetvalidator()
+    third_octet = convert2decimal(new_octet)
+    print(third_octet)
+    new_octet = octetvalidator()
+    fourth_octet = convert2decimal(new_octet)
+    print(fourth_octet)
+    print(f"The IPV4 address is {first_octet}.{second_octet}.{third_octet}.{fourth_octet}")
+
+def convert2decimal(new_octet):
+    new_dec = int(new_octet, 2) #converts the binary to decimal form using int()
+    return new_dec #returns the input to pass on for printing
+
+def subciphermain():
+    string = input("Enter the string: ")
+    action = input("Enter a Number: \n 1. Encode \n 2. Decode \n 3. Brute Force \n 4. Exit \n Option: ")
+    if action == "1":
+        result = subkeyencript(string)
+        print(f"Your encrypted message: {result}")
+        subciphermain()
+    elif action == "2":
+        result = subkeydecript(string)
+        print(f"Your decrypted message is: {result}")
+        subciphermain()
+    elif action == "3":
+        subbruteforce(string)
+        subciphermain()
+    elif action == "4":
+        print("Goodbye")
+    else:
+        print("I'm sorry, that's not a valid option.")
+        subciphermain()
 
 def hexmain():
     print("Choose from the following options:\n 1. Hex to Decimal\n 2. Hex to ASCII\n")
@@ -353,31 +377,10 @@ def hexmain():
         ascii_string = hex2ascii(new_hex)
         print("Hex value in ASCII: ", ascii_string)
 
-def rot13decode(string):
-    try:
-        return codecs.encode(string, 'rot13')
-    except Exception as e:
-        return f"Invalid Input: {e}"
-
 def rot13main():
     string = input("Enter a string to encode/decode: ")
     result = rot13decode(string)
     print(f"Your message is: {result}")
-
-def rot47decode(string):
-    try:
-        x = []
-        for i in range(len(string)):  # access characters by index
-            j = ord(string[i])  # set j to the corresponding ordinal number
-            if 33 <= j <= 126:  # checks for ROT47 range
-                # decode left 47, account for negative 33, wrap %94
-                x.append(chr(33 + ((j - 47 - 33) % 94)))
-            else:
-                x.append(string[i])  #adds non ROT47 characters without changes
-        return ''.join(x)  # turns the list into a string
-
-    except Exception as e:
-        return e
 
 def rot47main():
     string = input("Enter a ROT47 string: ")
@@ -413,7 +416,7 @@ def uestools():
         else:
             print("Invalid option")
 
-
+#-----------------------------------------------CODE EXECUTION----------------------------------------------------------
 print(banner)
 options()
 
